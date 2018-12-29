d401120697eec1a44a0a97affff67d77e4e4c70c
J <j@Js-MacBook-Pro.local>
2018-12-28 23:53:54 -0500

    Bug fix: Screen is now able to display with -n/--new user commands

--- p.py
+++ p.py
@@ -7,43 +7,13 @@ import curses.textpad
 from datetime import date
 import getpass
 
-class argParsing:
-    def __init__(self,parser):
-        self.parser = parser
-        parser.add_argument('-f',action="store_true",help="These are the files")
-        parser.add_argument('-n','--new',type=str,nargs='?',const="new_file.txt",help="Will make a new note with specified title")
-        parser.add_argument('-v','--view',action="store_true",help="View all of the lists you have made")
-        parser.add_argument('-md','--make_d',nargs="?",const="New-List-Group",help="Creates a new list-group with a defualt name of 'New-List-Group'")
-        parser.add_argument('-vf','--view-file',type=str,help="Takes in a string as an argument and will open a file for this string")
-        parser.add_argument('-d','--delete',nargs="*",type=str,help='Takes an argument of a string, representing a file name')
-        parser.add_argument('-e','-edit',type=str,help="Takes in a file name and begins in edit mode")
-    
-    def get_args(self):
-        args = parser.parse_args()
-        return args
+from argument_parse import argParsing
+from file_view import file_viewer
 
 
-class fileNavigation:
-    def __init__(self):
-        self.files = os.listdir('.')
-
-    
-    def delete_files(self, files):
-        for name in files:
-            #Find the file in the dir and then delete it
-            pass
-
-    def get_files(self):
-        return self.files
-
-    
-    def edit_file(self, files_name):
-        #Should open a file and be able to edit it
-        pass
-    
 
 class create_file:
-    def __init__(self,fileName):
+    def __init__(self,fileName,):
         self.file_content = ""
         self.fileName = fileName
         pass
@@ -52,10 +22,13 @@ class create_file:
     def saveState(self,filename,contents):
         print "The users file is " + filename
         print "the contents of the file will be " + contents
-
+        file = open(filename,'w+')
+        file.writelines(contents)
+        file.close()
 
 
     def main(self,stdscr):
+
         try:
 
             curses.start_color()
@@ -98,73 +71,15 @@ class create_file:
 
             tbWin = curses.newwin(48,98,1,width/2 - 49)
             tbWin.bkgd(curses.color_pair(5))
-            tb = curses.textpad.Textbox(tbWin,insert_mode=True)            
+            tb = curses.textpad.Textbox(tbWin,insert_mode=True)        
             tb.edit()
             tbWin.refresh()
+            tbWin.getch()
             self.file_content = tb.gather()
         finally:    #This is the end of the ui, This is where we begin to save the contents
             curses.endwin()
             self.saveState(self.fileName,self.file_content)
-
-
-
-class ascii_art_view:
-    def __init__(self,mode):
-        self.mode = mode
-        # file = open(mode,'r')
-        # lines = []
-        # #Each line will be a string that will be stored in the array
-        # for line in file.readlines:
-        #     lines.append(line)
-            
-
-    def open_art(self):
-        file = open(self. mode,'r')
-        lines = []
-        #Each line will be a string that will be stored in the array
-        for line in file.readlines():
-            lines.append(line)
-        return lines
-
-
-class file_viewer:
-    def __init__(self):
-        self.file_list = fileNavigation().get_files()
-        pass
-
-    def make_title(self,stdscr,width,height):
-        title_view = curses.newwin(10,width,0 ,0)
-        title_view.border(0)
-        title_view.refresh()
-        row = 2
-        for line in ascii_art_view("title.ascii").open_art():
-            title_view.addstr(row,width/2 - 25,str(line),curses.A_BLINK)
-            row+=1
-
-        title_view.refresh()
-
-    def main(self,stdscr):
-        try:
-            height, width = stdscr.getmaxyx()
-            self.make_title(stdscr,width, height)    #This will make the ascii art
-    
-            curses.init_pair(4,curses.COLOR_BLACK, curses.COLOR_CYAN)
-            field = curses.newwin(30,50,height/2-10,width/2 - 30)
-            field.border(0)
-            field.bkgd(curses.color_pair(4))
-            field.refresh()
-            row = 1
-            for name in self.file_list:
-                field.addstr(row,20,name.upper())
-                row += 2
-            field.refresh()
-            field.getch()
-        finally:
-            curses.endwin()
-            print "We out "
-
-
-
+            print "File has been created!"
 
 
 
@@ -180,7 +95,10 @@ if(args.view):
 #When the user has a certain flag enabled, this will be called
  
 #There is a bug here when trying to load the new screen with custom -n/--new flag
+#This bug has been fixed
 if(args.new):
-    curses.wrapper(create_file(args.new).main)
-
-
+    try:
+        file = open(str(args.new), 'r+')
+        print "This file already exists, to edit enter edit mode. See --help"
+    except:
+        curses.wrapper(create_file(args.new).main)
